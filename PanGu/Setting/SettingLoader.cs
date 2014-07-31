7c929d50a166fd0e323de523c4731f762f67958f
海眸1990 <linjian@ymail.com>
2014-07-31 15:25:33 +0800

    first commit

---
+++ PanGu/Setting/SettingLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanGu.Setting
+{
+    class SettingLoader
+    {
+        private void Load(string fileName)
+        {
+            PanGuSettings.Load(fileName);
+        }
+
+        public SettingLoader(string fileName)
+        {
+            Load(fileName);
+        }
+
+        public SettingLoader()
+        {
+            string fileName = PanGu.Framework.Path.GetAssemblyPath() + "PanGu.xml";
+            //Console.WriteLine("zzz:"+fileName);
+            Load(fileName);
+        }
+    }
+}
